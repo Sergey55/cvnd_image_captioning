177e7b762e5f8e831e444385017a0f537b483658
Sergey Andreev <sergey666andreev@gmail.com>
2020-08-14 16:23:53 +0600

    feat: Add implmented NN model

--- model.py
+++ model.py
@@ -2,11 +2,13 @@ import torch
 import torch.nn as nn
 import torchvision.models as models
 
-
 class EncoderCNN(nn.Module):
+    
     def __init__(self, embed_size):
         super(EncoderCNN, self).__init__()
+        
         resnet = models.resnet50(pretrained=True)
+        
         for param in resnet.parameters():
             param.requires_grad_(False)
         
@@ -21,12 +23,31 @@ class EncoderCNN(nn.Module):
         return features
     
 
-class DecoderRNN(nn.Module):
+class DecoderRNN(nn.Module): 
+    
     def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
-        pass
+        super(DecoderRNN, self).__init__()
+        
+        self.word_embeddings = nn.Embedding(vocab_size, embed_size)
+        
+        self.lstm = nn.LSTM(embed_size, hidden_size, num_layers=num_layers, batch_first=True)
+        
+        self.fc = nn.Linear(hidden_size, vocab_size)
     
     def forward(self, features, captions):
-        pass
+        
+        batch_size = features.size(0)
+
+        embedded_words = self.word_embeddings(captions[:, :-1])
+
+        lstm_input = torch.cat((features.view((batch_size, 1, -1)), embedded_words), dim=1)
+        
+        lstm_out, _ = self.lstm(lstm_input)
+
+        out = self.fc(lstm_out)
+        
+        return out
+        
 
     def sample(self, inputs, states=None, max_len=20):
         " accepts pre-processed image tensor (inputs) and returns predicted sentence (list of tensor ids of length max_len) "
