660e7479804a906d519c12f4ac46a92be9302220
Sergey Andreev <sergey666andreev@gmail.com>
2020-10-26 16:36:44 +0600

    feat: Add trainign script

---
+++ train.py
@@ -0,0 +1,31 @@
+from torchvision import transforms as transforms
+
+from datamodule import CocoDataModule
+from model import CaptioningModel
+
+from pytorch_lightning import Trainer
+from argparse import ArgumentParser
+
+def main(args):
+
+    transformations = transforms.Compose([
+        transforms.Resize(256),
+        transforms.RandomCrop(224), 
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor()])
+
+    dm = CocoDataModule(transformations, batch_size=32, num_workers=8)
+
+    vocab_size = len(dm.train_dataloader().dataset.vocab)
+
+    model = CaptioningModel(256, 256, vocab_size)
+
+    trainer = Trainer.from_argparse_args(args)
+    trainer.fit(model, dm)
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser = Trainer.add_argparse_args(parser)
+
+    main(parser.parse_args())
+
